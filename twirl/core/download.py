96258a7dfb3d9a2ed8deaaeb84820ce8c50c9c13
thewarsawpakt <smallperson07@gmail.com>
2021-02-12 21:55:56 -0600

    Prettified the output.

--- twirl/core/download.py
+++ twirl/core/download.py
@@ -5,9 +5,14 @@ def pkg(name):
     handle = Handle("/", "/var/lib/pacman")
     localdb = handle.get_localdb()
     package  = localdb.get_pkg(name)
-
-    for attr in dir(package):
-        if "__" not in attr and "files" not in attr:
-            print(f"{attr}: {getattr(package, attr)}")
-        elif "date" in attr:
-            print(f"{attr}: {time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(1347517370))}")
+    if not package:
+        print("Package not found.")
+        exit()
+    print(f"""Name: {name}
+Requires: {package.depends}
+License: {package.licenses[0]}
+Description: {package.desc}
+Hash: {package.md5sum}
+Optional Dependencies: {package.optdepends}
+Url: {package.url}
+""")
