12d1a18473175decafb1921991a75706e6cb57ec
Sophon96 <71684640+Sophon96@users.noreply.github.com>
2021-03-03 12:24:45 -0600

    Semi fix existing dep resolution

--- twirl/core/download.py
+++ twirl/core/download.py
@@ -2,22 +2,36 @@ from twirl.errors import PackageNotFoundError
 from twirl.core.debug import logging
 from twirl import localdb
 
-def pkg(name, version=None):
+def search_for_pkg(name):
+    potential_packages = localdb.search(name)
+    i = None
+    for i in potential_packages:
+        if name in i.provides:
+            break
+    return i
+
+def pkg(name, version=None, acc_deps=set()):
 
     package = localdb.get_pkg(name)
     if not package:
-        raise PackageNotFoundError(name)
+        package = search_for_pkg(name)
+        if package is None:
+            raise PackageNotFoundError(name)
     logging.info(f"Attempting to install {name} (version {package.version})")
     #for file in package.files:
     #    print(file[0])
 
     print(f"{package.name}: {package.depends}")
     for dep in package.depends:
-        print(dep)
-        if ">=" in dep:
-            pkg(dep.split(">=")[0], dep.split(">=")[1])
-        elif "=" in dep:
-            pkg(dep.split("=")[0], dep.split("=")[1])
-        elif ".so" in dep:
+        if dep in acc_deps:
             continue
-        else: pkg(dep)
+        else:
+            acc_deps.add(dep)
+        print(dep)
+        if ".so" in dep:
+            dep = dep.replace(".so", "")
+        if ">=" in dep:
+            pkg(dep.split(">=")[0], dep.split(">=")[1], acc_deps=acc_deps)
+        elif "=" in dep:
+            pkg(dep.split("=")[0], dep.split("=")[1], acc_deps=acc_deps)
+        else: pkg(dep, acc_deps=acc_deps)
